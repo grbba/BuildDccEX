b7172905344784e2a68b654b9039f73e7b24b83e
Gregor Baues <gr2bba@gmail.com>
2021-04-12 08:20:48 +0200

    added EthernetInterface.h

---
+++ patch/EthernetInterface.h
@@ -0,0 +1,70 @@
+/*
+ *  © 2020,Gregor Baues,  Chris Harlow. All rights reserved.
+ *  
+ *  This file is part of DCC-EX/CommandStation-EX
+ *
+ *
+ *  This is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  It is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with CommandStation.  If not, see <https://www.gnu.org/licenses/>.
+ * 
+ *  Ethernet Interface added by Gregor Baues
+ */
+
+#ifndef EthernetInterface_h
+#define EthernetInterface_h
+#if __has_include ( "config.h")
+  #include "config.h"
+#else
+  #warning config.h not found. Using defaults from config.example.h 
+  #include "config.example.h"
+#endif
+#include "DCCEXParser.h"
+#include <Arduino.h>
+#include <avr/pgmspace.h>
+#include <Ethernet.h>
+#include "RingStream.h"
+
+/**
+ * @brief Network Configuration
+ * 
+ */
+
+#define MAX_ETH_BUFFER 512
+#define OUTBOUND_RING_SIZE 2048
+
+class EthernetInterface {
+
+ public:
+     
+    static void setup();       
+    static void loop();
+    
+ // > grbba
+    bool isConnected() { return connected; };
+    static EthernetInterface *get() { return singleton; };
+    EthernetClient *getClient(int socket) { return &clients[socket]; };
+// grbba >
+
+ private:
+    static EthernetInterface * singleton;
+    bool connected;
+    EthernetInterface();
+    void loop2();
+    EthernetServer * server;
+    EthernetClient clients[MAX_SOCK_NUM];                // accept up to MAX_SOCK_NUM client connections at the same time; This depends on the chipset used on the Shield
+    uint8_t buffer[MAX_ETH_BUFFER+1];                    // buffer used by TCP for the recv
+    RingStream * outboundRing;
+  
+};
+
+#endif
